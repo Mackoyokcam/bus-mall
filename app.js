804a213b0e89826fd9461d76dec74cbe3d033668
mackoyokcam <mark.hangarter@gmail.com>
2017-07-25 14:43:53 -0700

    charts displaying at the end, images removed when charts display

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ function GenerateImage(name) {
   GenerateImage.all.push(this);
 }
 
-GenerateImage.numberOfPicturesDisplayed = 6;
+// Must be less than half the amount of images in img directory
+GenerateImage.numberOfPicturesDisplayed = 3;
 
 GenerateImage.maxClicks = 25;
 
@@ -42,9 +43,13 @@ GenerateImage.names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubbl
 
 GenerateImage.all = [];
 
+var barData = [];
+var percentages = [];
+
 // Create image objects
 for (var i = 0; i < GenerateImage.names.length; i++) {
   new GenerateImage(GenerateImage.names[i]);
+  barData[i] = 0;
 }
 
 // Handles clicking of images
@@ -56,6 +61,7 @@ function handleClick(e) {
     for (var i = 0; i < GenerateImage.all.length; i++) {
       if (e.target.alt === GenerateImage.all[i].name) {
         GenerateImage.all[i].clicked++;
+        barData[i] = GenerateImage.all[i].clicked;
         console.log('User clicked: ' + GenerateImage.all[i].name);
         i = GenerateImage.all.length;
       }
@@ -63,7 +69,7 @@ function handleClick(e) {
   }
 
   // End if max clicks
-  if (GenerateImage.currentClicks >= 25) {
+  if (GenerateImage.currentClicks >= GenerateImage.maxClicks) {
     //Disable Event Listeners
     for (i = 0; i < GenerateImage.imgElements.length; i++) {
       document.getElementById(GenerateImage.imgElements[i]).removeEventListener('click', handleClick);
@@ -76,14 +82,24 @@ function handleClick(e) {
       if(GenerateImage.all[i].viewed === 0) {
         percentage = 0;
       }
-      var liElement = document.createElement('li');
-      liElement.textContent = GenerateImage.all[i].name + ' has been viewed ' +
-        GenerateImage.all[i].viewed + ' times and has been clicked ' +
-        GenerateImage.all[i].clicked + ' times.' + '(' + percentage + '%)';
-      document.getElementById('results').appendChild(liElement);
+
+      percentages.push(percentage);
     }
 
     alert('You have completed the focus group! Please see results.');
+
+    //Remove Images
+    var deleteImages = document.getElementById('body');
+    deleteImages.removeChild(document.getElementById('images'));
+    deleteImages.removeChild(document.getElementById('h2'));
+
+    //Add charts with headers
+    var barHeader = document.getElementById('bar_header');
+    barHeader.textContent = 'Number of votes per item.';
+    var polarHeader = document.getElementById('polar_header');
+    polarHeader.textContent = 'Percentages based on views and votes';
+    drawBarGraph();
+    drawPolarArea();
     return true;
   }
 
@@ -121,3 +137,65 @@ for (i = 0; i < GenerateImage.imgElements.length; i++) {
 }
 
 handleClick(false);
+
+function drawBarGraph() {
+  var ctx = document.getElementById('bar_graph').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: GenerateImage.names,
+      datasets: [{
+        label: 'Number of Votes',
+        data: barData,
+        backgroundColor: randomColor({
+          count: 20}),
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      },
+      responsive: false,
+      animation: {
+        duration: 1000,
+        easing: 'easeOutBounce'
+      }
+    }
+  });
+}
+
+function drawPolarArea() {
+  var ctx = document.getElementById('polar_area').getContext('2d');
+  myPolar = new Chart(ctx,{
+    type: 'polarArea',
+    data: {
+      labels: GenerateImage.names, // titles array we declared earlier
+      datasets: [{
+        data: percentages, // votes array we declared earlier
+        backgroundColor: randomColor({
+          count: 20})
+      }]
+    },
+    options: {
+      responsive: false,
+      animation: {
+        duration: 1000,
+        easing: 'easeOutBounce'
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          max: 10,
+          min: 0,
+          stepSize: 1.0
+        }
+      }]
+    }
+  });
+  chartDrawn = true;
+}
